5e8afd4656108fe87edeac260334fa59a60d5c59
morenomp <rais2004pod@gmail.com>
2024-09-17 02:45:55 +0200

    Update responsive_menu.js

--- JS/responsive_menu.js
+++ JS/responsive_menu.js
@@ -8,9 +8,6 @@ const invertlogo = document.getElementById("invertResp");
 const line_one = document.getElementById("line_one");
 const line_two = document.getElementById("line_two");
 
-const scroll_one = document.getElementById("bodyAnimate_resto");
-const scroll_two = document.getElementById("bodyAnimate");
-
 const header_one = document.getElementById("menu_scroll");
 
 // console.log(abrir);
@@ -23,8 +20,8 @@ abrir.addEventListener("click", () => {
 	line_one.classList.toggle("activeCerrar_one")
 	line_two.classList.toggle("activeCerrar_two")
 
-	scroll_one.classList.toggle("desactiveScroll")
-	scroll_two.classList.toggle("desactiveScroll")
+	const scroll_one = document.getElementById("hiddenBody");
+	scroll_one.classList.toggle("desactiveScroll_one")
 
 	header_one.classList.toggle("desactiveMenuScroll")
 });
